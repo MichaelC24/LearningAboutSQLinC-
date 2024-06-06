c7cb6ba7ac5e7fcea41464c9afc39967a0da353a
Michael Clemons <Mclemons@tql.com>
2024-06-06 19:13:32 -0400

    Add project files.

---
+++ LearningAboutSQLinC#/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace FetchDataExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Server=LocalHost\sqlexpress01;Database=MichaelFirstDatabase;Trusted_Connection=True;";
+            string queryString = "SELECT Dates FROM dbo.Dates";
+
+            // Create and open a connection to SQL Server
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+
+                // Execute the command and read the data
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        // Get the values from the current row
+                        DateTime date = reader.GetDateTime(0);
+
+                        // Print the values
+                        Console.WriteLine($"Date: {date}");
+                    }
+                }
+            }
+
+            Console.WriteLine("Data retrieval completed.");
+            Console.ReadKey();
+        }
+    }
+}
