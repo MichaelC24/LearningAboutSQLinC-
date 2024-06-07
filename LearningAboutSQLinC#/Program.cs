45790bceab271e824e03f691c73a356e35474a25
Michael Clemons <Mclemons@tql.com>
2024-06-07 08:28:09 -0400

    Added in dateSearch Function

--- LearningAboutSQLinC#/Program.cs
+++ LearningAboutSQLinC#/Program.cs
@@ -8,8 +8,10 @@ namespace FetchDataExample
         static void Main(string[] args)
         {
             string connectionString = @"Server=LocalHost\sqlexpress01;Database=MichaelFirstDatabase;Trusted_Connection=True;";
-            string queryString = "SELECT Dates FROM dbo.Dates";
-
+            string queryString = "SELECT cast(Dates as date) as Dates FROM dbo.Dates";
+            
+           
+            List<DateTime> dateList = new List<DateTime>();
             // Create and open a connection to SQL Server
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
@@ -19,19 +21,46 @@ namespace FetchDataExample
                 // Execute the command and read the data
                 using (SqlDataReader reader = command.ExecuteReader())
                 {
+
+
+                    //List[] datesArray = new list[Dates];
                     while (reader.Read())
                     {
-                        // Get the values from the current row
-                        DateTime date = reader.GetDateTime(0);
+                        DateTime dates = reader.GetDateTime(0);
+                        dateList.Add(dates);
+                        
 
-                        // Print the values
-                        Console.WriteLine($"Date: {date}");
                     }
                 }
             }
 
-            Console.WriteLine("Data retrieval completed.");
-            Console.ReadKey();
+            DateTime[] datesArray = dateList.ToArray();
+
+            foreach (DateTime date in datesArray)
+            {
+                Console.WriteLine(date);
+            }
+            //Console.WriteLine("complete");
+
+            DateTime dateSearch = DateTime.MinValue;
+
+            Console.WriteLine("please insert a date");
+            string dateinput = Console.ReadLine();
+
+            if(DateTime.TryParse(dateinput, out dateSearch))
+            {
+                Console.WriteLine("date inputted incorrectly");
+            }
+
+            foreach (DateTime date in datesArray)
+            {
+                if(dateSearch == date)
+                {
+                    Console.WriteLine("here is the date");
+                    Console.WriteLine(date);
+                }
+            }
+
         }
     }
 }
